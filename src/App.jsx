7c2f5ffc128512365d932c78198cfd0a85eb7fb4
Mohammed Abded Hassani Idrissi <72313863+hiabed@users.noreply.github.com>
2025-04-20 22:00:55 +0000

    Portfolio preparing.

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,17 @@
 import './App.css';
 
+const mainStyle = {
+  width: "1920px",
+  height: "1022px",
+  backgroundColor: "grey"
+}
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src="Octocat.png" className="App-logo" alt="logo" />
-        <p>
-          GitHub Codespaces <span className="heart">♥️</span> React
-        </p>
-        <p className="small">
-          Edit <code>src/App.jsx</code> and save to reload.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </p>
-      </header>
+      <div className='main' style={mainStyle}>
+        
+      </div>
     </div>
   );
 }
