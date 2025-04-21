93c7712b58a007756e4406adb7345210f9b168bc
Mohammed Abded Hassani Idrissi <72313863+hiabed@users.noreply.github.com>
2025-04-21 22:15:32 +0000

    Portfolio ongoing

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ const mainStyle = {
   backgroundColor: "#909090",
   position: "relative",
   overflow: "hidden",
-  border: "blue solid 2px"
+  border: "blue solid 2px",
 }
 
 const myPic = {
@@ -20,19 +20,65 @@ const myPic = {
 }
 
 const app = {
+  minWidth: 1920,
   maxWidth: "100vw",
-  height: "100vh",
+  height: "200vh",
   backgroundColor: "grey",
   display: "flex",
   flexDirection: "column",
   alignItems: "center",
 }
 
+const navigation = {
+  color: "white",
+  width: 637,
+  height: 46,
+  // border: "1px red solid",
+  display: "flex",
+  justifyContent: "space-evenly",
+  alignItems: "center",
+  position: "fixed",
+  fontSize: 17,
+  left: "50%",
+  top: 40,
+}
+
+const contact = {
+  height: 46,
+  width: 147,
+  borderRadius: 23,
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+  color: "black",
+  backgroundColor: "white",
+  textDecoration: "none",
+  fontWeight: "bold",
+  fontSize: 15,
+}
+
 function App() {
   return (
     <div className="App" style={app}>
       <div className='main' style={mainStyle}>
-        <div className="myPicture" style={myPic}></div>
+        <div className="myPicture" style={myPic}>
+        </div>
+        <div className="navig" style={navigation}>
+            <a href="#About" style={{color: "white",textDecoration: "none", fontWeight: "bold"}}>About me</a>
+            <a href="#Skills" style={{color: "white",textDecoration: "none", fontWeight: "bold"}}>Skills</a>
+            <a href="#Portfolio" style={{color: "white",textDecoration: "none", fontWeight: "bold"}}>Portfolio</a>
+            <a href="#Contact" style={contact}>CONTACT ME</a>
+        </div>
+        <div className='per-infos'>
+          <h3>Hi, I am</h3>
+          <h1>Mohammed-Abed Hassani-Idrissi</h1>
+          <p>Software Developer</p>
+          <div className='socials'>
+            <div className='social'>@</div>
+            <div className='social'>Git</div>
+            <div className='social'>In</div>
+          </div>
+        </div>
       </div>
     </div>
   );
