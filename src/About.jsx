00ee7d301e867089208ca681c50f5292d7239b63
Mohammed Abded Hassani Idrissi <72313863+hiabed@users.noreply.github.com>
2025-04-22 21:30:17 +0000

    about ongoing

--- src/About.jsx
+++ src/About.jsx
@@ -4,6 +4,11 @@ const About = () => {
     return (
     <div id="About">
         <h1 className='about'>ABOUT ME</h1>
+        <p className="about-description">Nulla in velit a metus rhoncus tempus. Nulla congue nulla vel sem varius finibus.
+            Sed ornare sit amet lorem sed viverra.
+            In vel urna quis libero viverra facilisis ut ac est.
+        </p>
+        <p className='read-more'>EXPLORE</p>
     </div>
     )
 }
