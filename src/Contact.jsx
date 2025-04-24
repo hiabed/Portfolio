0dea5af6883f3b3ec631494e79ccf295daf3da81
Mohammed-Abed Hassani-Idrissi <mhassani@e1r10p5.1337.ma>
2025-04-24 22:33:44 +0100

    Contact ongoing

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -11,6 +11,17 @@ const Contact = () => {
                 In vel urna quis libero viverra facilisis ut ac est.
             </p>
             <div className="separator"></div>
+            <form id="contact-form">
+                <label htmlFor="name" className="sr-only">Name</label>
+                <input type="text" name="name" id="name" placeholder="ENTER YOUR NAME*" required/>
+                <label htmlFor="email" className="sr-only">Email</label>
+                <input type="email" name="email" id="email" placeholder="ENTER YOUR EMAIL*" required/>
+                <label htmlFor="phone" className="sr-only">Phone number</label>
+                <input type="tel" name="phone" id="phone" placeholder="PHONE NUMBER"></input>
+                <label htmlFor="message" className="sr-only">message</label>
+                <input name="message" id="message" placeholder="YOUR MESSAGE*" required></input>
+                <input type="submit" value="Submit" />
+            </form>
         </div>
     )
 }
