dd74932cb0c62a2c518dca43f00536d232e89703
hiabed <you@example.com>
2025-04-27 19:39:53 +0100

    completing projects part

--- src/Portfolio.jsx
+++ src/Portfolio.jsx
@@ -1,40 +1,48 @@
 import './Portfolio.css';
 
-const images = [
-    {}
-]
-
 const Portfolio = () => {
     const projectsData = [
         {
           image: 'pingpong.png',  // Add your actual image URLs here
-          description: 'Project 1 description',
-          buttons: ['Button 1', 'Button 2'],
+          title: 'Pong Game Platform',
+          description: `Using the SPA implementation, I built the entire front end of our Pong game platform from the ground up in Vanilla JS with a
+                        responsive UI using HTML, CSS, and Bootstrap that promised a smooth user experience. I also helped my four teammates in the
+                        other aspects of the process, and the linking phase was also a rich yet tedious experience.`,
+          buttons: ['DEMO', 'MORE'],
         },
         {
           image: 'server.webp',
-          description: 'Project 2 description',
-          buttons: ['Button 3', 'Button 4'],
+          title: 'Custom Web Server',
+          description: `Developed a custom web server inspired by Nginx, gaining comprehensive knowledge of HTTP protocols, server architecture, and
+                        socket programming. My primary responsibility was implementing the POST method, including support for binary data, chunked
+                        transfer encoding, multipart boundaries, and parsing client requests for accurate processing.`,
+          buttons: ['DEMO', 'MORE'],
         },
         {
           image: 'inception.webp',
-          description: 'Project 3 description',
-          buttons: ['Button 5', 'Button 6'],
+          title: 'Inception',
+          description: `Designed a dynamic WordPress Web App using Docker. Created custom Dockerfiles to define rules for building each container
+                        from scratch, adhering to the project's restriction on using pre-built images from the Docker Hub. Configured Nginx as the web
+                        server and MariaDB for database management, ensuring seamless deployment and scalability.`,
+          buttons: ['DEMO', 'MORE'],
         },
         {
           image: 'minishell.png',
-          description: 'Project 4 description',
-          buttons: ['Button 7', 'Button 8'],
+          title: 'Minishell',
+          description: 'It is a small UNIX Shell that resembles Bash.',
+          buttons: ['DEMO', 'MORE'],
         },
         {
           image: 'ecommerce.png',
-          description: 'Project 5 description',
-          buttons: ['Button 9', 'Button 10'],
+          title: 'E-commerce Website',
+          description: `I Developed the UI of an e-commerce website using React.js for the first time.`,
+          buttons: ['DEMO', 'MORE'],
         },
         {
           image: 'cub3d.webp',
-          description: 'Project 6 description',
-          buttons: ['Button 11', 'Button 12'],
+          title: "3D Game with C",
+          description: "We implemented functional 3D game using Raycasting technic with C, from scratch using only a minimal graphic library called minilibx",
+          buttons: ['DEMO', 'MORE'],
         },
       ];
     return (
@@ -49,14 +57,19 @@ const Portfolio = () => {
             <div className="projects">
                 {projectsData.map((project, index) => (
                     <div className="project" key={index}>
-                    <img src={project.image} alt={`Project ${index + 1}`} width="100%" height="100%" />
-                    {/* <p>{project.description}</p> */}
-                    {/* <div className="buttons">
-                        {project.buttons.map((buttonText, btnIndex) => (
-                        <button key={btnIndex}>{buttonText}</button>
-                        ))}
-                    </div> */}
-                    </div>
+                      <img src={project.image} alt={`Project ${index + 1}`} width="100%" height="100%" />
+                      <div className='to-show'>
+                        <div className='to-align'>
+                          <p>{project.title}</p>
+                          <p>{project.description}</p>
+                          <div className="buttons">
+                              {project.buttons.map((buttonText, btnIndex) => (
+                              <button key={btnIndex}>{buttonText}</button>
+                              ))}
+                          </div>
+                        </div>
+                      </div>
+                    </div>  
                 ))}
             </div>
             <div className="more-to-come">
