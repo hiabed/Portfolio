625cc3220f9fb5068f23ed8262e5e0a404c530a0
Mohammed Abded Hassani Idrissi <72313863+hiabed@users.noreply.github.com>
2025-05-07 20:03:22 +0000

    perticles removed

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,6 @@ import { useScroll, useMotionValueEvent } from "framer-motion";
 import React from "react";
 
 const mainStyle = {
-  // minWidth: 1920,
   width: "100%",
   height: "1022px",
   backgroundColor: "#D7D7D7",
@@ -50,10 +49,6 @@ const contact = {
   fontSize: 15,
 }
 
-import Particles from "react-tsparticles";
-import { useCallback } from "react";
-import { loadSlim } from "tsparticles-slim"; // if you are going to use `loadSlim`, install the "tsparticles-slim" package too.
-
 function App() {
   const { scrollY } = useScroll();
   const [scrolled, setScrolled] = React.useState(false);
@@ -63,19 +58,6 @@ function App() {
   });
 
   const [isOpened, setIsOpened] = React.useState(false);
-
-  const particlesInit = useCallback(async engine => {
-    console.log(engine);
-    // you can initiate the tsParticles instance (engine) here, adding custom shapes or presets
-    // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
-    // starting from v2 you can add only the features you need reducing the bundle size
-    //await loadFull(engine);
-    await loadSlim(engine);
-  }, []);
-
-  const particlesLoaded = useCallback(async container => {
-      await console.log(container);
-  }, []);
 
   return (
     <div className="App" style={app}>
