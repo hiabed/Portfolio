73503e7cb96e00771c5ee8d34540462b49d89933
Your Name <you@example.com>
2026-05-22 19:06:23 +0100

    cv updated and logo need to be circular.

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/Portfolio/',
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   test: {
     globals: true,
     environment: 'jsdom',
